f38fbe32fca332991a85e1ff28d3b5ee42f4d815
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-23 04:51:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,5 +22,13 @@ class CashRegister
     end
   end
   
-  def 
+  def apply_discount
+    if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."
+    else
+      return "There is no discount to apply."
+    end
+  end 
 end 
