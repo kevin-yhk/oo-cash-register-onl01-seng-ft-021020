39e42d34e877c183d33bfc21a5971e369bc2dad9
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-23 04:47:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,4 +2,10 @@ class CashRegister
   
   attr_accessor :total, :discount, :items, :last_transaction_amount
   
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
   
