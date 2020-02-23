44f92b0bdc86e3c1ec7880a006944394150c59ea
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-23 05:30:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,26 +9,26 @@ class CashRegister
     end
 
     def add_item(title, price, quantity = 1)
-        @price = price * quantity 
-        @total += @price 
-        if quantity > 1
-            counter = 0 
-            while counter < quantity
-                @items << title
-                counter += 1
-            end
-        else 
-            @items << title 
+      @price = price * quantity 
+      @total += @price 
+      if quantity > 1
+        x = 0 
+        while x < quantity
+          @items << title
+          x += 1
         end
+      else 
+        @items << title 
+      end
     end
 
     def apply_discount
-        if @discount > 0 
-            @total -= (@total * @discount)/100
-            return "After the discount, the total comes to $#{@total}."
-        elsif @discount == 0 
-            return "There is no discount to apply."
-        end
+      if @discount > 0 
+        @total -= (@total * @discount)/100
+        return "After the discount, the total comes to $#{@total}."
+      elsif @discount == 0 
+        return "There is no discount to apply."
+      end
     end
 
     def items
